5a914e23ffd22582dd4bd584d064149474bbafe7
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-10-04 20:48:37 +0700

    began to learn arrays

--- module-two.js
+++ module-two.js
@@ -1,26 +1,57 @@
-const fruits = ["apple", "peach", "pear", "banana"];
-const lastElementIndex=fruits.length-1;
-const lastElement = fruits[lastElementIndex];
+// const fruits = ["apple", "peach", "pear", "banana"];
+// const lastElementIndex=fruits.length-1;
+// const lastElement = fruits[lastElementIndex];
 
 
-const teams = ['Real', 'Barcelona', 'Milan'];
-const teamNameToFind = 'Barcelona';
-let message;
-for (const team of teams) {
-        if (team === teamNameToFind) {
-                message = 'There is a team that you are looking for';
-                break;
-        }
-        message = 'There is no such a team';
-} console.log(message);
+// const teams = ['Real', 'Barcelona', 'Milan'];
+// const teamNameToFind = 'Barcelona';
+// let message;
+// for (const team of teams) {
+//         if (team === teamNameToFind) {
+//                 message = 'There is a team that you are looking for';
+//                 break;
+//         }
+//         message = 'There is no such a team';
+// } console.log(message);
 
 
-const names = ['Ozone', 'Abtin', 'Zetta', 'Ashi', 'Tham'];
-const nameToFind = 'Ozone';
-let message;
-for (const name of names) {
-        if (name === nameToFind) {
-                message = 'there is such a name';
-                break;
-        } message = 'sorry,there is no such a name';
-} console.log(message);
+// const names = ['Ozone', 'Abtin', 'Zetta', 'Ashi', 'Tham'];
+// const nameToFind = 'Ozone';
+// let message;
+// for (const name of names) {
+//         if (name === nameToFind) {
+//                 message = 'there is such a name';
+//                 break;
+//         } message = 'sorry,there is no such a name';
+// } console.log(message);
+
+
+// function getExtremeElements(array) {
+
+// const firstElement = array[0];
+// const lastElement = array[array.length - 1];
+// return array = [firstElement, lastElement];
+// }
+
+
+// const colors = ['red', 'yellow', 'pink', 'green', 'purple', 'orange', 'blue'];
+// // console.table(colors);
+// // const lastIndex = colors.length - 1;
+// for (let i = 0; i <= colors.length-1; i += 1){
+//         console.log(colors[i]);
+// }
+
+
+// const colors = ['red', 'yellow', 'pink', 'green', 'purple', 'orange', 'blue'];
+// const lastIndex = colors.length - 1;
+// for (let i = 0; i <= lastIndex; i += 1){
+//         colors[i] += '-1';
+//         console.log(colors[i]);
+// }
+
+
+// if you do not need index or do not need to change element of array then use this:
+const colors = ['red', 'yellow', 'pink', 'green', 'purple', 'orange', 'blue'];
+for (const color of colors) {
+        console.log(color);
+}
