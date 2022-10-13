96b1811c7022ddcd7e524c3bb6ee1e3ecd8b8add
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-10-13 11:49:24 +0700

    Thursday

--- module-three.js
+++ module-three.js
@@ -23,4 +23,43 @@
 //     [inputName]:inputValue,
 // }
 // console.log(colorPickerData);
-// ------------------------------------------------------------------
+
+// ---------------------------------------------------------------
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+
+// for (const key in apartment){
+//     keys.push(key);
+//     values.push(apartment[key]);
+// }
+
+// ----------------------------------------------------------------------
+
+const keys = [];
+const values = [];
+
+const advert = {
+    service: "apt",
+};
+
+const apartment = Object.create(advert);
+apartment.descr = "Spacious apartment in the city center";
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
+
+if(apartment.hasOwnProperty(key)){
+    keys.push(key);
+    values.push(apartment[key]);
+}
+}
+
+// -------------------------------------------------------------------
+
