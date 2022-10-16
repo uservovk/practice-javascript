2151a6b51ed4e1f414143bbaa8de8f06fa50fc01
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-10-16 21:17:05 +0700

    added last task of module 3

--- module-three.js
+++ module-three.js
@@ -231,6 +231,45 @@
 
 // -------------------------------------------------------------------------------------
 
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    for(const potion of this.potions){
+      if (potion.name===newPotion.name) {
+      return `Error! Potion ${newPotion.name} is already in your inventory!`;
+    }
+    }
+
+    this.potions.push(newPotion);
+  },
+  removePotion(potionName) {
+    for(let i=0;i<this.potions.length;i+=1){
+      if (this.potions[i].name ===potionName) {
+      this.potions.splice(i, 1);
+    }
+    } 
+return `Potion ${potionName} is not in inventory!`; 
+  },
+  updatePotionName(oldName, newName) {
+    for(const potion of this.potions){
+      if(potion.name===oldName){
+        potion.name=newName;
+      }
+    }
+      return `Potion ${oldName} is not in inventory!`;
+  },
+};
+
+// -------------------------------------------------------------------------------------------
+
 // const cart = {
 //     items: [],
 //   getItems() {
