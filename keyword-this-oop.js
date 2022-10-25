d54d4f9605df7d6f89ed641d688d0ba1aec2ab80
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-10-25 21:42:57 +0700

    prototype

--- keyword-this-oop.js
+++ keyword-this-oop.js
@@ -47,3 +47,33 @@
 
 // fnA(user.getInfo);
 // ---------------------------------------------------------------------------------
+
+const Car = function ({brand,model,price}={}) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+};
+
+const myCar = new Car({
+    brand: 'Honda',
+    model: 'Civic',
+    price: 30000,
+});
+
+Car.prototype.changePrice = function (newPrice) {
+    this.price = newPrice;
+};
+myCar.changePrice(25000);
+
+const myCar2 = new Car({
+    brand: 'Toyota',
+    model: 'Corolla',
+    price: 35000,
+});
+
+const myCar3 = new Car({
+    brand: 'Subaru',
+    model: 'Forester',
+    price: 40000,
+});
+
