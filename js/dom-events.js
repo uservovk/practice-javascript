baebeb8c96d779aac1f51b3fa878f52d2c13c4c7
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-10-31 20:14:12 +0700

    DOM

---
+++ js/dom-events.js
@@ -0,0 +1,8 @@
+const navRef = document.querySelector('li');
+console.log(navRef);
+const navLinksRef = document.querySelectorAll('.site-nav_link');
+console.log(navLinksRef);
+
+const imgRef = document.querySelector('img');
+console.log(imgRef);
+imgRef.src = './photos/pexels-cats-coming-1444321.jpg';
