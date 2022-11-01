f583f58da48332ad6acbc9b474dc0b679213be62
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-11-01 19:51:35 +0700

    dom

--- js/dom-events.js
+++ js/dom-events.js
@@ -1,8 +1,41 @@
 const navRef = document.querySelector('li');
-console.log(navRef);
+// console.log(navRef);
 const navLinksRef = document.querySelectorAll('.site-nav_link');
-console.log(navLinksRef);
+// console.log(navLinksRef);
 
 const imgRef = document.querySelector('img');
-console.log(imgRef);
+// console.log(imgRef);
 imgRef.src = './photos/pexels-cats-coming-1444321.jpg';
+// -------------------------------------------------------
+// console.log(imgRef.getAttribute('alt'));
+// console.log(imgRef.getAttribute('src'));
+// console.log(imgRef.getAttribute('width'));
+// console.log(imgRef.height);
+// console.log(imgRef.hasAttribute('alt'));
+imgRef.removeAttribute('alt');
+imgRef.alt = 'Sweet kitten';
+// console.log(imgRef);
+
+
+const titleRef = document.querySelector('.page-title');
+// console.log(titleRef.textContent);
+titleRef.textContent = 'I am a little cupcake';
+
+const textRef = document.querySelector('.description');
+// console.log(textRef.textContent);
+
+
+// console.log(titleRef.classList);
+titleRef.classList.add('qweqwe');
+console.log(titleRef);
+titleRef.classList.remove('page-title');
+console.log(titleRef);
+titleRef.classList.toggle('main-title');
+console.log(titleRef);
+titleRef.classList.toggle('main-title');
+console.log(titleRef);
+titleRef.classList.replace('qweqwe', 'title');
+console.log(titleRef.classList.contains('qweqwe'));
+
+const currentPageUrl = '/portfolio';
+const linkRef = document.querySelector(`.site-nav_link[href='${ currentPageUrl }']`);
