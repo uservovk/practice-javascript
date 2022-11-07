4edf42eecb90eb1c7f796eb1075f6705dc68a949
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-11-07 21:35:09 +0700

    added addButton function

--- js/event-delegation.js
+++ js/event-delegation.js
@@ -3,5 +3,27 @@ const container = document.querySelector('.js-container');
 container.addEventListener('click', onClick);
 
 function onClick(event) { 
-    console.log(event.target);
+    if (event.target.nodeName !== 'BUTTON') {
+        return;
+    };
+    console.log(event.target.textContent);
+};
+
+
+
+
+const addBtnRef = document.querySelector('.js-add-btn');
+
+addBtnRef.addEventListener('click', onAddBtnRefClick);
+
+let labelCounter = 6;
+
+function onAddBtnRefClick() {
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.textContent = `Button ${labelCounter}`;
+    
+    container.appendChild(btn);
+    
+    labelCounter += 1;
 };
