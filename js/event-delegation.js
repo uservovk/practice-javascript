9ddffed7f0bf2b55fe25850273c6f377e660bf28
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-11-08 20:52:14 +0700

    delegation

--- js/event-delegation.js
+++ js/event-delegation.js
@@ -1,3 +1,5 @@
+//------------------------ADD BUTTON IN THE LIST-------------------
+
 const container = document.querySelector('.js-container');
 
 container.addEventListener('click', onClick);
@@ -8,9 +10,6 @@ function onClick(event) {
     };
     console.log(event.target.textContent);
 };
-
-
-
 
 const addBtnRef = document.querySelector('.js-add-btn');
 
@@ -26,4 +25,56 @@ function onAddBtnRefClick() {
     container.appendChild(btn);
     
     labelCounter += 1;
+};
+//-------ABLE TO CHOOSE ONE ITEM AND ADD TO IT CLASS----------------------------------------------
+
+// const tagsContainer = document.querySelector('.tags__js-container');
+
+// tagsContainer.addEventListener('click', onTagsContainerClick);
+
+// function onTagsContainerClick(event) { 
+//     let currentButton = null;
+
+//     if (event.target.nodeName !== 'BUTTON') {
+//         return;
+//     };
+    
+//     const currentActiveBtn = document.querySelector('.tags__btn-active');
+
+//     if (currentActiveBtn) {
+//         currentActiveBtn.classList.remove('tags__btn-active');
+//     };
+//     // currentActiveBtn?.classList.remove('tags__btn-active');
+
+//     const nextActiveBtn = event.target;
+//     nextActiveBtn.classList.add('tags__btn-active');
+
+//     currentButton = nextActiveBtn.dataset.value;
+//     console.log(currentButton);
+// };
+//-----------ABLE TO CHOOSE MORE THEN ONE ITEM AND ADD CLASS TO THEM-------------------------------------------
+
+const tagsContainer = document.querySelector('.tags__js-container');
+const selectedTags = new Set();
+
+tagsContainer.addEventListener('click', onTagsContainerClick);
+
+function onTagsContainerClick(event) {
+    if (event.target.nodeName !== 'BUTTON') {
+        return;
+    };
+
+    const btn = event.target;
+    const tag = event.target.dataset.value;
+    const isActive=btn.classList.contains('tags__btn-active');
+
+    if (isActive) {
+        selectedTags.delete(tag);
+    } else {
+        selectedTags.add(event.target.dataset.value);
+    }
+
+    event.target.classList.toggle('tags__btn-active');
+
+    console.log(selectedTags);
 };
